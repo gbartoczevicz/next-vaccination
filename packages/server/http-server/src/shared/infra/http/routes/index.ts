21e5685b7bea30ec44d4c78c8d19b45bf9909491
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-04-09 19:10:50 -0300

    feat: create EntityID
    Story: [ch49]

--- packages/server/http-server/src/shared/infra/http/routes/index.ts
+++ packages/server/http-server/src/shared/infra/http/routes/index.ts
@@ -1,6 +1,29 @@
 import { Router } from 'express';
 
+import { EntityID } from '@server/shared/src';
+
 const routes = Router();
+
+routes.get('/test-uuid', (_, response) => {
+  const idToCompare = new EntityID();
+
+  const toBeTrue = new EntityID(idToCompare.toValue());
+  const toBeFalse = new EntityID();
+
+  console.log({
+    idToCompare,
+    toBeTrue,
+    isEquals: toBeTrue.equals(idToCompare)
+  });
+
+  console.log({
+    idToCompare,
+    toBeFalse,
+    isEquals: toBeFalse.equals(idToCompare)
+  });
+
+  return response.sendStatus(200);
+});
 
 routes.get('/', (request, response) => {
   console.log(request);
