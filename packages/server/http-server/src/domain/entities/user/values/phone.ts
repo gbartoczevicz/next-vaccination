a4079cfe9ad2d7f041d52b1ee509dba161e8060b
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-04-19 21:52:13 -0300

    test: create phone test
    Story: [ch100]

--- packages/server/http-server/src/domain/entities/user/values/phone.ts
+++ packages/server/http-server/src/domain/entities/user/values/phone.ts
@@ -1,17 +1,15 @@
-import { Either, left, right, ValueObject } from '@server/shared';
+import { Either, left, right } from '@server/shared';
 import { InvalidUserPhone } from '@entities/user/errors';
 
-interface IUserPhoneProps {
-  value: string;
-}
+export class UserPhone {
+  private readonly phone: string;
 
-export class UserPhone extends ValueObject<IUserPhoneProps> {
   get value(): string {
-    return this.props.value;
+    return this.phone;
   }
 
-  constructor(props: IUserPhoneProps) {
-    super(props);
+  constructor(value: string) {
+    this.phone = value;
   }
 
   private static isValid(phone: string): boolean {
@@ -22,14 +20,14 @@ export class UserPhone extends ValueObject<IUserPhoneProps> {
 
   static create(phone: string): Either<InvalidUserPhone, UserPhone> {
     if (!phone) {
-      return left(new InvalidUserPhone('Phone number is required'))
+      return left(new InvalidUserPhone('Phone number is required'));
     }
 
     if (!this.isValid(phone)) {
-      return left(new InvalidUserPhone(`Phone number ${phone} is invalid`))
+      return left(new InvalidUserPhone(`Phone number ${phone} is invalid`));
     }
 
-    const userPhone = new UserPhone({ value: phone });
+    const userPhone = new UserPhone(phone);
 
     return right(userPhone);
   }
