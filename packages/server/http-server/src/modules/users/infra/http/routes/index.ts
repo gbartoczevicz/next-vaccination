936cb0b57bdb923c3fcdaa43eaf504723e80a1ba
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-05-02 11:38:43 -0300

    refactor: remove modules dir

--- packages/server/http-server/src/modules/users/infra/http/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import { createUserController } from '@modules/users/useCases/createUser';
-
-const routes = Router();
-
-routes.post('/users', (request, response) => createUserController.execute(request, response));
-
-export { routes as usersRoutes };
--- packages/server/http-server/src/modules/users/repositories/IUsersRepository.ts
+++
@@ -1,6 +0,0 @@
-import { User, UserEmail } from '@modules/users/entities';
-
-export interface IUsersRepository {
-  create(user: User): Promise<User>;
-  findByEmail(email: UserEmail): Promise<User | null>;
-}
