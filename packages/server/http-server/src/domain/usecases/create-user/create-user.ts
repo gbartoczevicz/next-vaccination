a2c06652e1b21587e88f45d0ddce48b5177809a6
pedromoraisf <pedro.morais1997@gmail.com>
2021-05-05 21:14:42 -0300

    feat: add correct instance for User

--- packages/server/http-server/src/domain/usecases/create-user/create-user.ts
+++ packages/server/http-server/src/domain/usecases/create-user/create-user.ts
@@ -1,6 +1,5 @@
 import { ICreateUserDTO } from '@usecases/create-user';
 import { User } from '@entities/user';
-import { UserEmail, UserPassword, UserPhone } from '@entities/user/values';
 import { IUsersRepository } from '@usecases/output-ports/repositories/users';
 import { AccountAlreadyExists, UserValidation } from '@usecases/create-user/errors';
 import { Either, left, right, UnexpectedError } from '@server/shared';
@@ -15,51 +14,16 @@ export class CreateUserUseCase {
   }
 
   async execute(request: ICreateUserDTO): Promise<Response> {
-    const userEmailOrError = UserEmail.create(request.email);
-    const userPhoneOrError = UserPhone.create(request.phone);
-    const userPasswordOrError = UserPassword.create({
-      password: request.password
-    });
-
-    if (userEmailOrError.isLeft()) {
-      return left(userEmailOrError.value);
-    }
-
-    if (userPhoneOrError.isLeft()) {
-      return left(userPhoneOrError.value);
-    }
-
-    if (userPasswordOrError.isLeft()) {
-      return left(userPasswordOrError.value);
-    }
-
-    const userEmail = userEmailOrError.value;
-
-    const doesAccountAlreadyExists = await this.usersRepository.findByEmail(userEmail);
-
-    if (doesAccountAlreadyExists) {
-      return left(new AccountAlreadyExists(userEmail.email));
-    }
-
     const userOrError = User.create({
-      name: request.name,
-      email: userEmail,
-      phone: userPhoneOrError.value,
-      password: userPasswordOrError.value
+      ...request,
+      password: {
+        password: request.password
+      }
     });
-
     if (userOrError.isLeft()) {
       return left(userOrError.value);
     }
 
-    const user = userOrError.value;
-
-    try {
-      await this.usersRepository.save(user);
-    } catch (err) {
-      return left(new UnexpectedError(err));
-    }
-
-    return right(user);
+    return right(userOrError.value);
   }
 }
