28acfafd04b263864932f68052263f1158da2ad7
pedromoraisf <pedro.morais1997@gmail.com>
2021-05-05 21:31:52 -0300

    test: ensure return left if email already in use

--- packages/server/http-server/src/domain/usecases/create-user/create-user.spec.ts
+++ packages/server/http-server/src/domain/usecases/create-user/create-user.spec.ts
@@ -1,7 +1,21 @@
 import { User } from '@entities/user';
 import { InvalidUserEmail } from '@entities/user/errors';
+import { left } from '@server/shared';
 import { CreateUserUseCase } from '@usecases/create-user';
+import { InfraError } from '@usecases/output-ports/errors';
 import { FakeUsersRepository } from '@usecases/output-ports/repositories/users/fake';
+
+const makeFixture = ({
+  name = 'any_correct_name',
+  email = 'any_correct_email@mail.com',
+  phone = '(99) 99999-9999',
+  password = 'any_correct_password'
+}) => ({
+  name,
+  email,
+  phone,
+  password
+});
 
 interface SutTypes {
   sut: CreateUserUseCase;
@@ -22,12 +36,7 @@ describe('Create User Use Case Unitary Tests', () => {
   it('should instance correct User', async () => {
     const { sut } = makeSut();
 
-    const testable = await sut.execute({
-      name: 'any_correct_name',
-      email: 'any_correct_email@mail.com',
-      phone: '(99) 99999-9999',
-      password: 'any_correct_password'
-    });
+    const testable = await sut.execute(makeFixture({}));
 
     const user = <User>testable.value;
 
@@ -41,14 +50,22 @@ describe('Create User Use Case Unitary Tests', () => {
   it('should return left if User params is invalid', async () => {
     const { sut } = makeSut();
 
-    const testable = await sut.execute({
-      name: 'any_correct_name',
-      email: 'any_wrong_email',
-      phone: '(99) 99999-9999',
-      password: 'any_correct_password'
-    });
+    const testable = await sut.execute(makeFixture({ email: 'any_wrong_email' }));
 
     expect(testable.isLeft()).toBeTruthy();
     expect(testable.value).toEqual(new InvalidUserEmail('E-mail any_wrong_email is invalid'));
+  });
+
+  it('should return left if email already in use', async () => {
+    const { sut, fakeUsersRepository } = makeSut();
+
+    const spyFakeUsersRepository = jest.spyOn(fakeUsersRepository, 'findByEmail');
+    spyFakeUsersRepository.mockImplementation(() => Promise.resolve(left(new InfraError('any_infra_error'))));
+
+    const testable = await sut.execute(makeFixture({}));
+
+    expect(spyFakeUsersRepository).toHaveBeenCalledTimes(1);
+    expect(testable.isLeft()).toBeTruthy();
+    expect(testable.value).toEqual(new InfraError('any_infra_error'));
   });
 });
