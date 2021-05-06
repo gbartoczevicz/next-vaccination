dc02260da4adf4b90c1f31f7a8204fdfd2c5f34c
pedromoraisf <pedro.morais1997@gmail.com>
2021-05-05 21:06:00 -0300

    refactor: set async for methods

--- packages/server/http-server/src/domain/usecases/output-ports/repositories/users/users.ts
+++ packages/server/http-server/src/domain/usecases/output-ports/repositories/users/users.ts
@@ -1,7 +1,9 @@
 import { User } from '@entities/user';
 import { UserEmail } from '@entities/user/values';
+import { InfraError } from '@usecases/output-ports/errors';
+import { Either } from '@server/shared';
 
 export interface IUsersRepository {
-  save(user: User): Promise<User>;
-  findByEmail(email: UserEmail): Promise<User | null>;
+  save(user: User): Promise<Either<InfraError, User>>;
+  findByEmail(email: UserEmail): Promise<Either<InfraError, User | null>>;
 }
