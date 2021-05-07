42d01bd5e22aba8553f910e4ca4601eec0312c83
pedromoraisf <pedro.morais1997@gmail.com>
2021-05-07 09:46:56 -0300

    temp: only for ci tests

--- packages/server/http-server/src/external/encrypter/fake.ts
+++ packages/server/http-server/src/external/encrypter/fake.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Encrypter } from '@entities/output-ports/encrypter';
 
+//
 export class FakeEncrypter implements Encrypter {
   async encrypt(toEncrypt: string): Promise<string> {
     return Promise.resolve(toEncrypt);
