caecb7d0670c142e84bc307fe0a15067c46696f6
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-05-07 20:16:33 -0300

    feat: create patients repository port
    Story: [ch120]

---
+++ packages/server/http-server/src/domain/usecases/output-ports/repositories/patients/fake.ts
@@ -0,0 +1,27 @@
+import { Patient } from '@entities/patient';
+import { User } from '@entities/user';
+import { Either, EntityID, right } from '@server/shared';
+import { InfraError } from '@usecases/output-ports/errors';
+import { IPatientsRepository } from './patients';
+
+export class FakePatientsRepository implements IPatientsRepository {
+  async save(patient: Patient): Promise<Either<InfraError, Patient>> {
+    return Promise.resolve(right(patient));
+  }
+
+  async findByDocument(document: string): Promise<Either<InfraError, Patient | null>> {
+    const fixture = Patient.create({
+      birthday: new Date(),
+      document,
+      user: User.create({
+        id: new EntityID(),
+        name: 'name',
+        email: 'user@email.com',
+        phone: '9999-9999',
+        password: { password: 'secret' }
+      }).value as User
+    }).value as Patient;
+
+    return Promise.resolve(right(fixture));
+  }
+}
