9e961191c69729cb3df3c3125d3ed187ea607cd5
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-05-09 13:49:01 -0300

    feat: create health professional repo
    Story: [ch75]

---
+++ packages/server/http-server/src/domain/usecases/output-ports/repositories/health-professional/health-professional.ts
@@ -0,0 +1,11 @@
+import { HealthProfessional } from '@entities/health-professional';
+import { Either } from '@server/shared';
+import { InfraError } from '@usecases/output-ports/errors';
+
+export type FindUnique = Either<InfraError, HealthProfessional | null>;
+export type Save = Either<InfraError, HealthProfessional>;
+
+export interface IHealthProfessionalRepository {
+  findByDocument(document: string): Promise<FindUnique>;
+  save(healthProfessional: HealthProfessional): Promise<Save>;
+}
---
+++ packages/server/http-server/src/domain/usecases/output-ports/repositories/health-professional/index.ts
@@ -0,0 +1,2 @@
+export * from './health-professional';
+export * from './fake';
