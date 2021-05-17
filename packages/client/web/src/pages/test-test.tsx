b644467ea4c77b68b5fa7ad6dcb487b37e29a7a0
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-05-17 20:26:33 -0300

    feat: setup icon
    Story: [ch126]

--- packages/client/web/src/pages/test-test.tsx
+++ packages/client/web/src/pages/test-test.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
-import { Heading } from '@chakra-ui/react';
+import { Flex, Heading, Icon } from '@chakra-ui/react';
+import { MdLocalHospital as Hospital } from 'react-icons/md';
 
-const TestTest: React.FC = () => <Heading>Routing test page</Heading>;
+const TestTest: React.FC = () => (
+  <>
+    <Heading>Routing test page</Heading>
+
+    <Flex>
+      <Icon as={Hospital} w={8} h={8} color="red.500" />
+    </Flex>
+  </>
+);
 
 export default TestTest;
