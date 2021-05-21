652834ed945f673e4824b5430895ab43fe3177da
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-05-21 18:19:52 -0300

    feat: create coordinate entity
    Story: [ch124]

---
+++ packages/server/http-server/src/domain/entities/vaccination-point/values/coordinate.ts
@@ -0,0 +1,32 @@
+import { Either, left, right } from '@server/shared';
+import { InvalidCoordinate } from '../errors';
+
+interface ICoordinateProps {
+  latitude: number;
+  longitude: number;
+}
+
+export class Coordinate {
+  readonly latitude: number;
+
+  readonly longitude: number;
+
+  constructor(latitude: number, longitude: number) {
+    this.latitude = latitude;
+    this.longitude = longitude;
+  }
+
+  static create(props: ICoordinateProps): Either<InvalidCoordinate, Coordinate> {
+    if (!props.latitude) {
+      return left(new InvalidCoordinate('Latitude is required'));
+    }
+
+    if (!props.longitude) {
+      return left(new InvalidCoordinate('Longitude is required'));
+    }
+
+    const coordinate = new Coordinate(props.latitude, props.longitude);
+
+    return right(coordinate);
+  }
+}
