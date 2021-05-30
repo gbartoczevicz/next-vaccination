a6033797a02c604611ffa42085222311ff06ce88
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-05-29 23:05:09 -0300

    refactor: get vaccination points usecase
    Story: [ch136]

--- packages/server/http-server/src/domain/usecases/get-nearby-vaccination-points/get-nearby-vaccination-points.ts
+++ packages/server/http-server/src/domain/usecases/get-nearby-vaccination-points/get-nearby-vaccination-points.ts
@@ -4,33 +4,19 @@ import { Coordinate } from '@entities/vaccination-point/values';
 import { Either, left, right } from '@server/shared';
 import { UserNotFound } from '@usecases/errors';
 import { InfraError } from '@usecases/output-ports/errors';
-import { IUsersRepository } from '@usecases/output-ports/repositories/users';
 import { IVaccinationPointsRepository } from '@usecases/output-ports/repositories/vaccination-points';
 import { IGetNearbyVaccinationPointsDTO } from './dto';
 
 type Response = Either<InfraError | UserNotFound | InvalidCoordinate, VaccinationPoint[]>;
 
 export class GetNearbyVaccinationPointsUseCase {
-  private usersRepository: IUsersRepository;
-
   private vaccintaionPointsRepository: IVaccinationPointsRepository;
 
-  constructor(usersRepository: IUsersRepository, vaccintaionPointsRepository: IVaccinationPointsRepository) {
-    this.usersRepository = usersRepository;
+  constructor(vaccintaionPointsRepository: IVaccinationPointsRepository) {
     this.vaccintaionPointsRepository = vaccintaionPointsRepository;
   }
 
   async execute(request: IGetNearbyVaccinationPointsDTO): Promise<Response> {
-    const doesUserExistsOrError = await this.usersRepository.findById(request.userId);
-
-    if (doesUserExistsOrError.isLeft()) {
-      return left(doesUserExistsOrError.value);
-    }
-
-    if (!doesUserExistsOrError.value) {
-      return left(new UserNotFound());
-    }
-
     const coordinateOrError = Coordinate.create(request.coordinate);
 
     if (coordinateOrError.isLeft()) {
