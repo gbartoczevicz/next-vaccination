0baf8c7345d3a383a2247d1c60e08cde9d0430ea
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-05-30 12:07:45 -0300

    refactor: remove date-parser from project
    Story: [ch137]

--- packages/server/http-server/src/domain/entities/output-ports/date-parser/date-parser.ts
+++
@@ -1,9 +0,0 @@
-export interface IDateParser {
-  /**
-   * @description Format to UTC with 00:00 offset (Zulu)
-   * and set time to 00:00:00.
-   *
-   * Zulu: https://greenwichmeantime.com/time-zone/gmt-plus-0
-   */
-  parseToUTCZuluTime(dateToParse: Date): Date;
-}
