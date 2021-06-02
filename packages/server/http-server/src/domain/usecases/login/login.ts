71f616bb1bf2a7cd3c8cb0b9d7fd92e6fa404317
pedromoraisf <pedro.morais1997@gmail.com>
2021-06-02 20:15:20 -0300

    feat: add return right with User entitiy if result has been succeded

--- packages/server/http-server/src/domain/usecases/login/login.ts
+++ packages/server/http-server/src/domain/usecases/login/login.ts
@@ -1,11 +1,11 @@
 import { User } from '@entities/user';
-import { Either, left } from '@server/shared';
+import { Either, left, right } from '@server/shared';
 import { AccountNotFinded } from '@usecases/errors/account-not-finded';
 import { InfraError } from '@usecases/output-ports/errors';
 import { IUsersRepository } from '@usecases/output-ports/repositories/users';
 import { ILoginDTO } from './dto';
 
-type Response = Either<InfraError, User>;
+type Response = Either<AccountNotFinded | InfraError, User>;
 
 export class LoginUseCase {
   private readonly usersRepository: IUsersRepository;
@@ -22,5 +22,7 @@ export class LoginUseCase {
       const handledError = !requestedAccount.value ? new AccountNotFinded() : requestedAccount.value;
       return left(handledError);
     }
+
+    return right(requestedAccount.value);
   }
 }
