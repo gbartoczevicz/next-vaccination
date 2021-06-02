a5f1ad508480d3c2dc4a24a8ad9f196486a3cff3
pedromoraisf <pedro.morais1997@gmail.com>
2021-06-02 19:43:35 -0300

    feat: add call email and password infrastructure query correctly

--- packages/server/http-server/src/domain/usecases/login/login.ts
+++ packages/server/http-server/src/domain/usecases/login/login.ts
@@ -1,22 +1,20 @@
 import { User } from '@entities/user';
-import { Either, left } from '@server/shared';
-import { WrongProperty } from '@usecases/errors/wrong-property';
+import { Either } from '@server/shared';
 import { InfraError } from '@usecases/output-ports/errors';
+import { IUsersRepository } from '@usecases/output-ports/repositories/users';
 import { ILoginDTO } from './dto';
 
 type Response = Either<InfraError, User>;
 
 export class LoginUseCase {
-  async execute(request: ILoginDTO): Promise<Response> {
-    const hasWrongValueInRequest = Object.entries(request).filter((it) => {
-      const propValue = it[1];
-      return [null, undefined, '', false].includes(propValue);
-    });
+  private readonly usersRepository: IUsersRepository;
 
-    if (hasWrongValueInRequest.length) {
-      const isolateWrongProps = hasWrongValueInRequest.map((it) => it[0]);
-      const wrongProps = isolateWrongProps.join(' and ');
-      return left(new WrongProperty(wrongProps));
-    }
+  constructor(usersRepository: IUsersRepository) {
+    this.usersRepository = usersRepository;
+  }
+
+  async execute(request: ILoginDTO): Promise<Response> {
+    const { user, password } = request;
+    const requestedAccount = await this.usersRepository.findByEmailAndPassword(user, password);
   }
 }
