9f19e520d54f245d63b093d19885e718999b1287
pedromoraisf <pedro.morais1997@gmail.com>
2021-06-21 20:16:39 -0300

    feat: add validate payload

---
+++ packages/server/http-server/src/adapters/helpers/validate-payload/validate-payload.ts
@@ -0,0 +1,38 @@
+import { ValidatePayload } from '@adapters/helpers/validate-payload';
+
+export class ValidatePayloadHelper implements ValidatePayload {
+  private requiredFields: Array<string>;
+
+  private payload: Array<string>;
+
+  setPayload(payload: any = {}): ValidatePayload {
+    const isNotObject = typeof payload !== 'object' || payload === null;
+    const proxiedPayload = !isNotObject ? payload : {};
+
+    this.payload = Object.keys(proxiedPayload);
+
+    return this;
+  }
+
+  setRequiredFields(requiredFields: Array<string>): ValidatePayload {
+    this.requiredFields = requiredFields;
+
+    return this;
+  }
+
+  containsAllRequiredFields(): boolean {
+    const missingRequiredFields = this.requiredFields.some((requiredField) => {
+      return !this.payload.includes(requiredField);
+    });
+
+    return !missingRequiredFields;
+  }
+
+  exibeMissingFields(): Array<string> {
+    const missingFields = this.requiredFields.filter((requiredField) => {
+      return !this.payload.includes(requiredField);
+    });
+
+    return missingFields;
+  }
+}
