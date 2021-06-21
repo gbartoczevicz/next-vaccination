60cc2aef9607dc3b606cd0026e987d05534e9c06
pedromoraisf <pedro.morais1997@gmail.com>
2021-06-21 20:16:51 -0300

    test: ensure validate payload efficacy

---
+++ packages/server/http-server/src/adapters/helpers/validate-payload/validate-payload.spec.ts
@@ -0,0 +1,72 @@
+import { ValidatePayloadHelper } from '@adapters/helpers/validate-payload';
+
+interface SutTypes {
+  sut: ValidatePayloadHelper;
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new ValidatePayloadHelper();
+
+  return {
+    sut
+  };
+};
+
+describe('ValidatePayload Util', () => {
+  describe('containsAllRequiredFields >> should be validate if not contains required fields in payload', () => {
+    test('should returns true if payload is a not object', () => {
+      const { sut } = makeSut();
+
+      sut.setRequiredFields(['resultCode', 'message']);
+
+      expect(sut.setPayload('').containsAllRequiredFields()).toBe(false);
+    });
+
+    test('should returns true', () => {
+      const { sut } = makeSut();
+
+      sut.setRequiredFields(['resultCode', 'message']);
+
+      const testablePayload = {
+        resultCode: 200,
+        message: 'any_message'
+      };
+
+      expect(sut.setPayload(testablePayload).containsAllRequiredFields()).toBe(true);
+    });
+
+    test('should returns false', () => {
+      const { sut } = makeSut();
+
+      sut.setRequiredFields(['resultCode', 'message']);
+
+      const testablePayload = {
+        message: 'any_message'
+      };
+
+      expect(sut.setPayload(testablePayload).containsAllRequiredFields()).toBe(false);
+    });
+  });
+
+  describe('exibeInvalidFields >> should be returned an string with property names are missing', () => {
+    test('should returns all required fields if payload is a not object', () => {
+      const { sut } = makeSut();
+
+      sut.setRequiredFields(['resultCode', 'message']);
+
+      expect(sut.setPayload('').exibeMissingFields()).toEqual(['resultCode', 'message']);
+    });
+
+    test('should be returns resultCode property', () => {
+      const { sut } = makeSut();
+
+      sut.setRequiredFields(['resultCode', 'message']);
+
+      const testablePayload = {
+        message: 'any_message'
+      };
+
+      expect(sut.setPayload(testablePayload).exibeMissingFields()).toEqual(['resultCode']);
+    });
+  });
+});
